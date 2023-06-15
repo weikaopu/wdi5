57821b639aea23b0d5eb6e53c88a01fc022326aa
dominikfeininger <dominik.feininger@js-soft.com>
2023-06-15 17:01:12 +0200

    chore(wdioV8): cleanup example js app

--- examples/ui5-js-app/e2e-test-config/wdio-webserver-cjs.conf.js
+++
@@ -1,9 +0,0 @@
-const { baseConfig } = require("./wdio.base.conf")
-const merge = require("deepmerge")
-
-const _config = {
-    specs: ["../webapp/test/e2e/basic.test.js"],
-    baseUrl: "http://localhost:8888"
-}
-
-exports.config = merge(baseConfig, _config)
