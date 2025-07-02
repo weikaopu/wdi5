6f326bd69d6d40e2faec72cfeafea623064dbf4a
I586943 <jimmy.xu03@sap.com>
2025-07-02 19:07:13 +0800

    feat(auth): update, add cjs esm ([#510]( https://github.com/ui5-community/wdi5/issues/510))

---
+++ esm/lib/authentication/BTPAuthenticator.js
@@ -0,0 +1,62 @@
+import Authenticator from "./Authenticator.js";
+class BTPAuthenticator extends Authenticator {
+    disableBiometricAuth;
+    idpDomain;
+    idpDomainOpt;
+    constructor(options, browserInstanceName) {
+        super(browserInstanceName);
+        this.usernameSelector = options.usernameSelector ?? "#j_username";
+        this.passwordSelector = options.passwordSelector ?? "#j_password";
+        this.submitSelector = options.submitSelector ?? "#logOnFormSubmit";
+        this.idpDomainOpt = options.idpDomain;
+        this.disableBiometricAuth = options.disableBiometricAuthentication ?? false;
+        if (this.disableBiometricAuth) {
+            if (!options.idpDomain) {
+                throw new Error("idpDomain is required when disableBiometricAuthentication is set to `true` your wdi5.conf.(j|t)s");
+            }
+            this.idpDomain = options.idpDomain;
+        }
+    }
+    async disableBiometricAuthentication() {
+        await this.browserInstance.setCookies({
+            name: "skipPasswordlessAuthnDeviceConfig",
+            value: "true",
+            domain: this.idpDomain
+        });
+    }
+    async login() {
+        const idpDomainOpt = this.idpDomainOpt;
+        if (!(await this.getIsLoggedIn())) {
+            // e.g. <a href="saml/discovery?returnIDParam=idp&amp;entityID=https://integrationtest.authentication.eu10.hana.ondemand.com&amp;idp=abybs5za0.accounts.ondemand.com&amp;isPassive=true" class="saml-login-link">SAP IAS</a>
+            const targetIdpEle = await this.browserInstance.waitUntil(async function () {
+                return await $(`a[href*="idp=${idpDomainOpt}"]`);
+            }, {
+                timeout: 10000,
+                timeoutMsg: `expected idp: ${idpDomainOpt} can not be retrieved after 10s.`
+            });
+            if (!!targetIdpEle) {
+                targetIdpEle.click();
+            }
+            const usernameControl = await this.browserInstance.$(this.usernameSelector);
+            const passwordControl = await this.browserInstance.$(this.passwordSelector);
+            const submit = await this.browserInstance.$(this.submitSelector);
+            const wdi5Username = this.getUsername();
+            const wdi5Password = this.getPassword();
+            if (await passwordControl.isExisting()) {
+                await usernameControl.setValue(wdi5Username);
+                await passwordControl.setValue(wdi5Password);
+                await submit.click();
+            }
+            else {
+                await usernameControl.setValue(wdi5Username);
+                await submit.click();
+                const password = await this.browserInstance.$(this.passwordSelector);
+                await password.setValue(wdi5Password);
+                await submit.click();
+            }
+            this.setIsLoggedIn(true);
+        }
+    }
+}
+export default BTPAuthenticator;
+//# sourceMappingURL=BTPAuthenticator.js.map
